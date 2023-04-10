2189a150d4789f41e719a6004c89ab8993e3a2c8
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-04-10 15:02:48 +0700

    menambahkan void inputMK

--- Class1_oop_162.cpp
+++ Class1_oop_162.cpp
@@ -15,5 +15,21 @@ class Matakuliah {
 private:
 	int sks;
 	string kode;
-	string namaMK
+	string namaMK;
+public:
+	void inputMK() {
+		
+		cout << "\nMasukkan jumlah SKS= ";
+		cin >> sks;
+		cout << "Masukkan Kode MK= ";
+		cin >> kode;
+		cout << "Masukkan Nama MK= ";
+		cin >> namaMK;
+	}
+
+	void tampilMK() {
+		cout << "\nJumlah sks= " << sks;
+		cout << "\nKode MK= " << kode;
+		cout << "nNama MK= " << namaMK;
+	}
 };
